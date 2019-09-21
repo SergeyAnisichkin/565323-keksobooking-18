c633eb4d5160896dbe51c779a9e246f4146f43bf
Sergey Anisichkin <s.anisichkin@gmail.com>
2019-09-21 18:44:28 +0300

    Поправил замечания ДЗ 5

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+'use strict';
+
 var NOTICES_AMOUNT = 8;
 var OFFER_TITLE = 'Заголовок предложения';
 var MIN_PRICE = 1;
 var MAX_PRICE = 10000;
 var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var MIN_ROOMS = 1;
-var MAX_ROOMS  = 4;
+var MAX_ROOMS = 4;
 var MIN_GUESTS = 1;
 var MAX_GUESTS = 4;
 var CHECK_TIMES = ['12:00', '13:00', '14:00'];
@@ -12,22 +14,22 @@ var OFFER_FEATURES = ['wifi', 'flat', 'dishwasher', 'parking', 'washer', 'elevat
 var OFFER_DESCRIPTION = 'Описание предложения';
 var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var MIN_Y_LOCATION = 130;
-var MAX_Y_LOCATION  = 630;
+var MAX_Y_LOCATION = 630;
 
-var map = document.querySelector(".map");
-map.classList.remove("map--faded");
-var mapPin = document.querySelector(".map__pin");
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+var mapPin = document.querySelector('.map__pin');
 var minXLocation = mapPin.clientWidth / 2;
 var maxXLocation = map.clientWidth - mapPin.clientWidth / 2;
-var pinHeight = mapPin.clientHeight;
+var mapPins = map.querySelector('.map__pins');
+var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-var notices = [];
-for (var i = 0; i < NOTICES_AMOUNT; i++) {
-  var xLocation  = Math.floor(minXLocation + Math.random() * (maxXLocation - minXLocation));
-  var yLocation  = Math.floor(MIN_Y_LOCATION + Math.random() * (MAX_Y_LOCATION - MIN_Y_LOCATION));
-  notices[i] = {
+var createNotice = function (index) {
+  var xLocation = Math.floor(minXLocation + Math.random() * (maxXLocation - minXLocation));
+  var yLocation = Math.floor(MIN_Y_LOCATION + Math.random() * (MAX_Y_LOCATION - MIN_Y_LOCATION));
+  var notice = {
     author: {
-      avatar: 'img/avatars/user0' + (i + 1) + '.png'
+      avatar: 'img/avatars/user0' + (index + 1) + '.png'
     },
     location: {
       x: xLocation,
@@ -47,22 +49,32 @@ for (var i = 0; i < NOTICES_AMOUNT; i++) {
       photos: OFFER_PHOTOS.slice(Math.floor(Math.random() * OFFER_PHOTOS.length))
     }
   };
-}
-
-var mapPins = map.querySelector('.map__pins');
-var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-
-var renderNotices = function (notices) {
-  var noticesElement = mapPinTemplate.cloneNode(true);
-  noticesElement.style.left = Math.floor(notices.location.x - mapPin.clientWidth / 2) + 'px';
-  noticesElement.style.top = Math.floor(notices.location.y - mapPin.clientHeight) + 'px';
-  noticesElement.querySelector('img').src = notices.author.avatar;
-  noticesElement.querySelector('img').alt = notices.offer.title;
-  return noticesElement;
+  return notice;
 };
 
-var fragment = document.createDocumentFragment();
-for (i = 0; i < notices.length; i++) {
-  fragment.appendChild(renderNotices(notices[i]));
-}
-mapPins.appendChild(fragment);
+var generateNotices = function (noticesAmount) {
+  var notices = [];
+  for (var i = 0; i < noticesAmount; i++) {
+    notices[i] = createNotice(i);
+  }
+  return notices;
+};
+
+var createPinNotice = function (notice) {
+  var noticeElement = mapPinTemplate.cloneNode(true);
+  noticeElement.style.left = Math.floor(notice.location.x - mapPin.clientWidth / 2) + 'px';
+  noticeElement.style.top = Math.floor(notice.location.y - mapPin.clientHeight) + 'px';
+  noticeElement.querySelector('img').src = notice.author.avatar;
+  noticeElement.querySelector('img').alt = notice.offer.title;
+  return noticeElement;
+};
+
+var createFragment = function (notices) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < notices.length; i++) {
+    fragment.appendChild(createPinNotice(notices[i]));
+  }
+  return fragment;
+};
+
+mapPins.appendChild(createFragment(generateNotices(NOTICES_AMOUNT)));
