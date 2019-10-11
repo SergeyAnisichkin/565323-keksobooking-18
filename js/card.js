570ad43c04a524347d2b6d777ccf3746af5b3986
Sergey Anisichkin <s.anisichkin@gmail.com>
2019-10-11 21:35:48 +0300

    Выполнил ДЗ 11

---
+++ js/card.js
@@ -0,0 +1,52 @@
+'use strict';
+
+window.card = (function () {
+
+  var mapCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+
+  var changeFeaturesList = function (featuresNode, featuresNotice) {
+    var copyChild = featuresNode.firstElementChild;
+    while (featuresNode.querySelector('.popup__feature')) {
+      featuresNode.removeChild(featuresNode.querySelector('.popup__feature'));
+    }
+    for (var i = 0; i < featuresNotice.length; ++i) {
+      copyChild.setAttribute('class', 'popup__feature popup__feature--' + featuresNotice[i]);
+      featuresNode.appendChild(copyChild.cloneNode(true));
+    }
+    return featuresNode;
+  };
+
+  var addPhotosList = function (photosNode, photosNotice) {
+    var copyChild = photosNode.querySelector('.popup__photo');
+    photosNode.removeChild(copyChild);
+    for (var i = 0; i < photosNotice.length; ++i) {
+      copyChild.src = photosNotice[i];
+      photosNode.appendChild(copyChild.cloneNode(true));
+    }
+    return photosNode;
+  };
+
+  var createCardNotice = function (notice) {
+    var cardElement = mapCardTemplate.cloneNode(true);
+    cardElement.querySelector('.popup__title').textContent = notice.offer.title;
+    cardElement.querySelector('.popup__text--address').textContent = notice.offer.address;
+    cardElement.querySelector('.popup__text--price').textContent = notice.offer.price + '₽/ночь';
+    cardElement.querySelector('.popup__type').textContent = window.data.getOfferTypeValue(notice.offer.type);
+    var textCapacity = notice.offer.rooms + ' комнаты для ' + notice.offer.guests + ' гостей';
+    cardElement.querySelector('.popup__text--capacity').textContent = textCapacity;
+    var textTime = 'Заезд после ' + notice.offer.checkin + ', выезд до ' + notice.offer.checkout;
+    cardElement.querySelector('.popup__text--time').textContent = textTime;
+    changeFeaturesList(cardElement.querySelector('.popup__features'), notice.offer.features);
+    cardElement.querySelector('.popup__description').textContent = notice.offer.description;
+    addPhotosList(cardElement.querySelector('.popup__photos'), notice.offer.photos);
+    cardElement.querySelector('.popup__avatar').src = notice.author.avatar;
+    return cardElement;
+  };
+
+  return {
+    createCard: function (notice) {
+      return createCardNotice(notice);
+    }
+  };
+
+})();
